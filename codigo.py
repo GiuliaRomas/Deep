aa90bba901e782303fe18e81153d49e3531a4dde
Giulia <49544670+GiuliaRomas@users.noreply.github.com>
2024-04-06 11:56:37 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,2 +1,8 @@
 import streamlit as st
-st.title("Direito")
+#st.title("Pequisa por embasamento legal")
+st.set_page_config(page_title="Pequisa por embasamento legal", page_icon=":book:", layout="wide")
+
+if "messages" not in st.session_state:
+  st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+  
