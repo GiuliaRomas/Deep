321916501d1f3d7ea12872138120758d5123a724
Giulia <49544670+GiuliaRomas@users.noreply.github.com>
2024-04-06 12:45:59 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,7 +1,27 @@
-#from openai import OpenAI
-import streamlit as st
+# Set up the Streamlit app layout
+st.title("🤖 ChatGPT with Memory 🧠")
+#st.subheader(" Powered by 🦜 LangChain + OpenAI + Streamlit")
 
-st.title("Pequisa por embasamento legal")
-#st.set_page_config(page_title="Pequisa por embasamento legal", page_icon=":book:", layout="wide")
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
 
-st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+# Let user select version
+st.write("升级调试中，暂不可用")
+st.write("GPT4.5 Turbo 上线了！无需注册就可以体验只有OpenAI付费用户才可以体验的GPT4.5了！")
+version = st.selectbox("Choose ChatGPT version 请选择您想使用的ChatGPT版本", ("3.5", "4.5"))
+if version == "3.5":
+    # Use GPT-3.5 model
+    MODEL = "gpt-3.5-turbo"
+else:
+    # USe GPT-4.5 model
+    MODEL = "gpt-4"
+    
+# Ask the user to enter their OpenAI API key
+#API_O = st.sidebar.text_input("API-KEY", type="password")
+# Read API from Streamlit secrets
+API_O = st.secrets["OPENAI_API_KEY"]
