e1ed34eb23c5170359a9942162e06e6bef9a6568
Giulia <49544670+GiuliaRomas@users.noreply.github.com>
2024-04-06 12:42:40 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -5,8 +5,3 @@ st.title("Pequisa por embasamento legal")
 st.set_page_config(page_title="Pequisa por embasamento legal", page_icon=":book:", layout="wide")
 
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
