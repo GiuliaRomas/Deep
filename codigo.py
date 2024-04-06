10f4a91a53e4367bbb187b55509c1ce73ac66d5f
Giulia <49544670+GiuliaRomas@users.noreply.github.com>
2024-04-06 12:43:29 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -2,6 +2,6 @@
 import streamlit as st
 
 st.title("Pequisa por embasamento legal")
-st.set_page_config(page_title="Pequisa por embasamento legal", page_icon=":book:", layout="wide")
+#st.set_page_config(page_title="Pequisa por embasamento legal", page_icon=":book:", layout="wide")
 
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
