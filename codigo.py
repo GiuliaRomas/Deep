b613fbc9a01a58c0a62784bb6dd0ba82e01e3176
Giulia <49544670+GiuliaRomas@users.noreply.github.com>
2024-04-06 12:41:44 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,4 +1,6 @@
+from openai import OpenAI
 import streamlit as st
+
 st.title("Pequisa por embasamento legal")
 st.set_page_config(page_title="Pequisa por embasamento legal", page_icon=":book:", layout="wide")
 with st.sidebar:
